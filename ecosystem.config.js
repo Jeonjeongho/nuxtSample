c3c341db0b9d18337cbcc2cd5ba9f55c1d9aedac
jeonjeongho <wjswjdgh757@naver.com>
2021-06-02 20:59:28 +0900

    기본포트 변경

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'NuxtAppName',
+        port: 80,
         exec_mode: 'cluster',
         instances: 'max', // Or a number of instances
         script: './node_modules/nuxt/bin/nuxt.js',
